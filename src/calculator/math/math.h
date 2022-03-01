ea9d217fa80a156ebf5dbde496b79046301831b3
xgurka00 <xgurka00@stud.fit.vutbr.cz>
2022-03-01 12:47:58 +0000

    Added math library interface and .editorconfig

---
+++ src/calculator/math/math.h
@@ -0,0 +1,14 @@
+/* Copyright 2022 Alexaj Ondrej, Gurka Oliver, Magda Jakub */
+
+#include <cmath>
+
+class Math {
+    public:
+    double add(double a, double b);
+    double subtract(double a, double b);
+    double multiply(double a, double b);
+    double divide(double a, double b);
+    unsigned long long int factorial(unsigned long int a);
+    long double power(double a, double n);
+    long double root(double a, double n);
+};
