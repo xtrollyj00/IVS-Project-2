4c926d0b45ffb9c11952dbe9f64b6db5d1c550f1
OndrejAlexaj <alexajondrej@gmail.com>
2022-04-10 21:55:19 +0000

    GUI done (but without displaying computed answer, will be done in another branch)

--- src/calculator/gui/mainwindow.cpp
+++ src/calculator/gui/mainwindow.cpp
@@ -22,6 +22,15 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     connect(ui->pushButton_0, SIGNAL(released()), this, SLOT(on_pushButton_digit_released()));
+    connect(ui->pushButton_1, SIGNAL(released()), this, SLOT(on_pushButton_digit_released()));
+    connect(ui->pushButton_2, SIGNAL(released()), this, SLOT(on_pushButton_digit_released()));
+    connect(ui->pushButton_3, SIGNAL(released()), this, SLOT(on_pushButton_digit_released()));
+    connect(ui->pushButton_4, SIGNAL(released()), this, SLOT(on_pushButton_digit_released()));
+    connect(ui->pushButton_5, SIGNAL(released()), this, SLOT(on_pushButton_digit_released()));
+    connect(ui->pushButton_6, SIGNAL(released()), this, SLOT(on_pushButton_digit_released()));
+    connect(ui->pushButton_7, SIGNAL(released()), this, SLOT(on_pushButton_digit_released()));
+    connect(ui->pushButton_8, SIGNAL(released()), this, SLOT(on_pushButton_digit_released()));
+    connect(ui->pushButton_9, SIGNAL(released()), this, SLOT(on_pushButton_digit_released()));
 }
 
 MainWindow::~MainWindow()
@@ -29,72 +38,297 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::on_pushButton_digit_released()
-{   
+void MainWindow::variablesSet()
+{
+    writingEnable = true;
+    wasOperation = true;
+    stillZeroFirst = false;
+    secondOperand = true;
+    digitPressed = false;
+}
 
+void MainWindow::on_pushButton_digit_released()
+{
+    if(writingEnable && (ui->label->text()).size()<MAX_WIDTH){
+        digitPressed = true;
+        QPushButton * signalSender = (QPushButton*)sender();
+
+        if(!secondOperand)
+        {
+            if(stillZeroFirst && signalSender->text() == "0")
+                return;
+            if(stillZeroFirst && !usedDot_1)
+                ui->label->setText("");
+            if(signalSender->text() != "0")
+                stillZeroFirst = false;
+        }
+        else
+        {
+            if(startOfOperand && signalSender->text() == "0")
+            {
+                stillZeroFirst = true;
+            }
+            else if(stillZeroFirst && signalSender->text() == "0")
+                return;
+            else if(stillZeroFirst && !usedDot_2)
+                return;
+            else if(signalSender->text() != "0")
+                stillZeroFirst = false;
+        }
+
+        operatorNotAllowed = false;
+        startOfOperand = false;
+        nothingPressed = false;
+        ui->label->setText(ui->label->text() + signalSender->text());
+    }
 }
 
 void MainWindow::on_pushButton_dot_released()
 {
-
+    if(!usedDot_1 && !secondOperand && writingEnable && digitPressed && (ui->label->text()).size()<MAX_WIDTH)
+    {
+        ui->label->setText(ui->label->text() + ".");
+        usedDot_1 = true;
+        stillZeroFirst = false;
+        operatorNotAllowed = true;
+    }
+    else if(!usedDot_2 && secondOperand && writingEnable && digitPressed && (ui->label->text()).size()<MAX_WIDTH)
+    {
+        ui->label->setText(ui->label->text() + ".");
+        usedDot_2 = true;
+        stillZeroFirst = false;
+        operatorNotAllowed = true;
+    }
 }
 
 void MainWindow::on_pushButton_equals_released()
 {
-
+    // TODO
 }
 
 void MainWindow::on_pushButton_ans_released()
 {
-
+    // Can't concatenate  with result of "789Ans" for example
+    if(startOfOperand && !secondOperand)
+    {
+        ui->label->setText("Ans");
+        usedDot_1 = true;
+        stillZeroFirst = false;
+        writingEnable = false;
+        startOfOperand = false;
+    }
+    else if(startOfOperand && secondOperand)
+    {
+        ui->label->setText(ui->label->text() + "Ans");
+        usedDot_2 = true;
+        stillZeroFirst = false;
+        writingEnable = false;
+        startOfOperand = false;
+    }
 }
 
 void MainWindow::on_pushButton_times_released()
 {
-
+    if(!wasOperation && !operatorNotAllowed && (ui->label->text()).size()<MAX_WIDTH)
+    {
+        ui->label->setText(ui->label->text() + "*");
+        wasOperation = true;
+        stillZeroFirst = false;
+        secondOperand = true;
+        startOfOperand = true;
+        digitPressed = false;
+    }
 }
 
 void MainWindow::on_pushButton_plus_released()
 {
-
+    if(!wasOperation && !operatorNotAllowed && (ui->label->text()).size()<MAX_WIDTH)
+    {
+        ui->label->setText(ui->label->text() + "+");
+        variablesSet();
+        startOfOperand = true;
+    }
 }
 
 void MainWindow::on_pushButton_div_released()
 {
-
+    if(!wasOperation && !operatorNotAllowed && (ui->label->text()).size()<MAX_WIDTH)
+    {
+        ui->label->setText(ui->label->text() + "/");
+        variablesSet();
+        startOfOperand = true;
+    }
 }
 
 void MainWindow::on_pushButton_minus_released()
 {
-
+    if(!wasOperation && !operatorNotAllowed && (ui->label->text()).size()<MAX_WIDTH)
+    {
+        ui->label->setText(ui->label->text() + "-");
+        variablesSet();
+        startOfOperand = true;
+    }
 }
 
 void MainWindow::on_pushButton_fact_released()
 {
-
+    if(!wasOperation && !operatorNotAllowed && (ui->label->text()).size()<MAX_WIDTH)
+    {
+        ui->label->setText(ui->label->text() + "!");
+        variablesSet();
+        writingEnable = false;
+    }
 }
 
 void MainWindow::on_pushButton_power_released()
 {
-
+    if(!wasOperation && !operatorNotAllowed && (ui->label->text()).size()<MAX_WIDTH)
+    {
+        ui->label->setText(ui->label->text() + "^");
+        variablesSet();
+        startOfOperand = true;
+    }
 }
 
 void MainWindow::on_pushButton_root_released()
 {
-
+    if(!wasOperation && !operatorNotAllowed && (ui->label->text()).size()<MAX_WIDTH)
+    {
+        ui->label->setText(ui->label->text() + "√");
+        variablesSet();
+        startOfOperand = true;
+    }
 }
 
 void MainWindow::on_pushButton_log_released()
 {
-
+    if(!wasOperation && !operatorNotAllowed && nothingPressed)
+    {
+        ui->label->setText("log");
+        variablesSet();
+    }
 }
 
 void MainWindow::on_pushButton_delete_released()
 {
+    QString operations = "+-*/!^√";
+    QString displayed = ui->label->text();
 
+    if(displayed.length() != 1)
+    {
+        QChar removed = displayed.back();
+        displayed.chop(1);
+        QChar toBeRemovedNext = displayed.back();
+
+        // If length of displayed after removal is 1,
+        // it needs to be checked if 0 is next char to be removed.
+        // If yes, variables describing this state are set
+        if(displayed.length() == 1)
+        {
+            if(toBeRemovedNext == "0")
+            {
+                stillZeroFirst = true;
+                digitPressed = true;
+                nothingPressed = true;
+            }
+        }
+
+        // If removed char was operation, variables are set to
+        // state corresponding with no operation
+        if(operations.contains(removed, Qt::CaseSensitive))
+        {
+            wasOperation = false;
+            writingEnable = true;
+            startOfOperand = false;
+            secondOperand = false;
+            digitPressed = true;
+        }
+        // If "g" was removed, it could be "log",
+        // therefore following 2 chars ("og") are also removed
+        // and variables are set to state like nothing was typed
+        else if(removed == "g")
+        {
+            displayed.chop(2);
+            wasOperation = false;
+            stillZeroFirst = true;
+            startOfOperand = true;
+            secondOperand = false;
+            digitPressed = true;
+            nothingPressed = true;
+            ui->label->setText("0");
+            return;
+        }
+        else if(removed == ".")
+        {
+            operatorNotAllowed = false;
+            if(secondOperand)
+                usedDot_2 = false;
+            else
+                usedDot_1 = false;
+        }
+
+        // When char, that could poosibly be remvoed next time, is
+        // operation, variables are set to corresponding state
+        if(operations.contains(toBeRemovedNext, Qt::CaseSensitive))
+        {
+            startOfOperand = true;
+            secondOperand = true;
+            stillZeroFirst = false;
+            digitPressed = false;
+        }
+
+        // When char, that could poosibly be remvoed next time, is
+        // dot, means something like xx. is on display, therefore
+        // operator is not allowed
+        if(toBeRemovedNext == ".")
+        {
+            operatorNotAllowed = true;
+        }
+
+        // If operation was typed, we need to check which char
+        // could be removed next next time. Because in case it is
+        // operation, we need to check which char follows the operation.
+        // If it is "0", we need to be aware of that (in case of
+        // typing new chars not to allow something like 789+000 or 789+035)
+        if(wasOperation)
+        {
+            QChar toBeRemovedNextNext = displayed[displayed.length()-2];
+            if(operations.contains(toBeRemovedNextNext, Qt::CaseSensitive))
+            {
+                if(toBeRemovedNext == "0")
+                {
+                    secondOperand = true;
+                    stillZeroFirst = true;
+                }
+            }
+        }
+
+        ui->label->setText(displayed);
+    }
+
+    else
+    {
+        wasOperation = false;
+        stillZeroFirst = true;
+        startOfOperand = true;
+        secondOperand = false;
+        digitPressed = true;
+        nothingPressed = true;
+        ui->label->setText("0");
+    }
 }
 
 void MainWindow::on_pushButton_clear_released()
 {
-
+    ui->label->setText("0");
+    stillZeroFirst = true;
+    secondOperand = false;
+    startOfOperand = true;
+    wasOperation = false;
+    writingEnable = true;
+    digitPressed = true;
+    usedDot_1 = false;
+    usedDot_2 = false;
+    nothingPressed = true;
+    operatorNotAllowed = false;
 }
--- src/calculator/gui/mainwindow.h
+++ src/calculator/gui/mainwindow.h
@@ -18,6 +18,8 @@
 
 #include <QMainWindow>
 
+#define MAX_WIDTH 31
+
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -27,82 +29,119 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
+    /**
+     * @brief Ceates main window
+     */
     MainWindow(QWidget *parent = nullptr);
+
+    /**
+     * @brief Destroys main window
+     */
     ~MainWindow();
 
 private:
+    void variablesSet();
+
     Ui::MainWindow *ui;
+    bool stillZeroFirst = true; /**< Indicates if 0 is still the beginning of the operand */
+    bool secondOperand = false; /**< Indicates if secondOperand is currently being typed */
+    bool startOfOperand = true; /**< Indicates if the operand is goind to gain it's first digit */
+    bool usedDot_1 = false; /**< Indicates if dot was used in 1st operand */
+    bool usedDot_2 = false; /**< Indicates if dot was used in 2nd operand */
+    bool wasOperation = false; /**< Indicates if operation button was used, to not allow any other */
+    bool writingEnable = true; /**< Indicates when is writing enabled (when factorial is typed -> false) */
+    bool digitPressed = true; /**< Indicates if some digit was pressed */
+    bool operatorNotAllowed = false; /**< Indicates if it is possible to type operator */
+    bool nothingPressed = true; /**< Indicates if no input was given from user (or all has been cleared/deleted) */
 
 private slots:
     /**
     * @brief Concatenates digits
+    * @detail Does not support 000000xxx or 0789 type of input
+    * but 0.xxx is supported
     */
     void on_pushButton_digit_released();
 
     /**
     * @brief Appends "." to what is displayed
+    * @detail More dots within one digit string
+    * are not supported (for example 0.12.00 not supp.).
+    * Keeps track of dots (in 1st and 2nd operand).
+    * Does not allow expr. like 8.+9.
+    * Using 2 variables indicating
+    * if in operand (1st or 2nd) is dot already
     */
     void on_pushButton_dot_released();
 
     /**
     * @brief Sends operands to evaluation in form of string
-    * Formats in which are operations sent:
+    * @detail Formats in which are operations sent:
     * "154.2+32" ... similarly in case of -,*,/
-    * "xROOTy" means xth root of number y
+    * "x√y" means xth root of number y
     * "x!" the format for sending factorials.
     * "x^y" means x to the power of y
     * "logX" means decimal logarigthm of number X
+    * "Ans+xxx" or "Ans+Ans" or "xxx+Ans"
+    * @todo implementation waiting for interface from file between GUI and math lib.
     */
     void on_pushButton_equals_released();
 
     /**
-    * @brief Loads and displays last shown result
+    * @brief Loads and displays last shown result under "Ans"
+    * @detail Just format of Ans+X, Ans+Ans, X+Ans, logAns or Ans! is supported,
+    * where "+" represents any operator.
     */
     void on_pushButton_ans_released();
 
     /**
-    * @brief Adds * to string know it's multiplication
+    * @brief If possible, adds * to string to know it's multiplication
     */
     void on_pushButton_times_released();
 
     /**
-    * @brief Adds + to string to know sum is desired
+    * @brief If possible, adds + to string to know sum is desired
     */
     void on_pushButton_plus_released();
 
     /**
-    * @brief Adds / to string to know division is desired
+    * @brief If possible, adds / to string to know division is desired
     */
     void on_pushButton_div_released();
 
     /**
-    * @brief Adds - to string to know subtracting is desired
+    * @brief If possible,adds - to string to know subtracting is desired
     */
     void on_pushButton_minus_released();
 
     /**
-    * @brief Adds ! to string to know it's factorial
+    * @brief If possible, adds ! to string to know it's factorial
     */
     void on_pushButton_fact_released();
 
     /**
-    * @brief Adds ^ to string to know it's power
+    * @brief If possible, adds ^ to string to know it's power
     */
     void on_pushButton_power_released();
 
     /**
-    * @brief Adds keyword "ROOT" to specify operation
+    * @brief If possible, adds keyword "ROOT" to specify operation
     *
     */
     void on_pushButton_root_released();
 
     /**
-    * @brief Adds keyword "log" to specify operation
+    * @brief If possible, adds keyword "log" to specify operation
+    * @detail Works only when nothing else was typed.
     */
     void on_pushButton_log_released();
 
     /**
-    * @brief Deletes the last entered symbol
+    * @brief Deletes symbols from the right
+    * @detail When deleting last symbol, 0
+    * is placed (just like on the start).
+    * The "log" is deleted all at once (all 3 chars)
+    * and 0 is placed instead.
+    * The "ans" is also deleted all at once (all 3 chars)
     */
     void on_pushButton_delete_released();
 
